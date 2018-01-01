2d8aaa87cc5f71e0deaf1e51e000fc4d1f511e61
Sam Desborough <sam.desborough@gmail.com>
2018-01-01 16:55:47 +0000

    remove flag crap

--- app/app.go
+++ app/app.go
@@ -1,13 +1,10 @@
 package app
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	loads "github.com/go-openapi/loads"
-	flag "github.com/spf13/pflag"
 
 	"github.com/desbo/fixtures/restapi"
 	"github.com/desbo/fixtures/restapi/operations"
@@ -19,25 +16,9 @@ func init() {
 		log.Fatalln(err)
 	}
 
-	var server *restapi.Server // make sure init is called
-
-	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, "Usage:\n")
-		fmt.Fprint(os.Stderr, "  tabletennis365-com-fixtures-server [OPTIONS]\n\n")
-
-		title := "tabletennis365.com fixtures"
-		fmt.Fprint(os.Stderr, title+"\n\n")
-		desc := "tabletennis365.com fixtures"
-		if desc != "" {
-			fmt.Fprintf(os.Stderr, desc+"\n\n")
-		}
-		fmt.Fprintln(os.Stderr, flag.CommandLine.FlagUsages())
-	}
-	// parse the CLI flags
-	flag.Parse()
+	var server *restapi.Server
 
 	api := operations.NewTabletennis365ComFixturesAPI(swaggerSpec)
-	// get server with flag values filled out
 	server = restapi.NewServer(api)
 
 	defer server.Shutdown()
